2c96ef6de422ce80ece77c943b7565376ea205b2
Denis Chernykh <forgamesandforum@yandex.ru>
2026-02-22 23:31:39 +0500

    feat(app): wire requests page with new header

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from '@/App.tsx'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import '@/app/styles/index.css'
 import { AppChakraProvider } from '@/app/providers/chakra-provider'
 
---
+++ src/pages/requests/ui/requests-page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/widgets/header'
+import { Box } from '@chakra-ui/react'
+
+export function RequestsPage() {
+  return (
+    <Box minH="100vh" bg="bg.canvas">
+      <Header />
+    </Box>
+  )
+}
