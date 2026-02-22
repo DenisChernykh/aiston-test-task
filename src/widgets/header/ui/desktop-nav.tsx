ea2042a3895c05d8096cdbdabb9e347286489621
Denis Chernykh <forgamesandforum@yandex.ru>
2026-02-22 23:31:16 +0500

    feat(header): implement desktop and mobile header widgets

---
+++ src/widgets/header/ui/desktop-nav.tsx
@@ -0,0 +1,26 @@
+import { ChevronDownIcon } from '@/shared/assets/icons'
+import { Box, HStack, Text } from '@chakra-ui/react'
+
+export function DesktopNav() {
+  return (
+    <HStack gap="35">
+      <HStack gap="14">
+        <Text color="text.primary" fontSize="lg" lineHeight="md" fontWeight="regular">
+          Заявки
+        </Text>
+        <Text color="text.muted" fontSize="lg" lineHeight="md" fontWeight="regular">
+          Отчеты
+        </Text>
+      </HStack>
+
+      <HStack gap="4" color="text.muted">
+        <Text fontSize="lg" lineHeight="md" fontWeight="regular">
+          Справочники
+        </Text>
+        <Box as="span" display="inline-flex" alignItems="center" justifyContent="center" mt="2">
+          <ChevronDownIcon w="7px" h="5px" color="currentColor" />
+        </Box>
+      </HStack>
+    </HStack>
+  )
+}
